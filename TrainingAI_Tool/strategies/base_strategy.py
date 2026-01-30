9f68ad81fde30c1e14b4bd9213d89ae9c6204e4d
DESKTOP-EC248AS\Admin <viet.nv1990@gmail.com>
2026-01-30 12:08:26 +0700

    remove

--- TrainingAI_Tool/strategies/base_strategy.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-import pandas as pd
-
-class BaseStrategy(ABC):
-    @abstractmethod
-    def analyze(self, df: pd.DataFrame) -> dict:
-        """Nhận DataFrame -> Trả về JSON kết quả"""
-        pass
